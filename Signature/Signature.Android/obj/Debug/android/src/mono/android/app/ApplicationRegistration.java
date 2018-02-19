2ad1d17ad5cf1cbd0a3778d6b605c8f9831692c4
Eleazar Saavedra <eleazarsaavedra@gmail.com>
2018-02-18 21:12:45 -0430

    Signature

---
+++ Signature/Signature.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Signature.Droid.MainApplication, Signature.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md54f2f657d11db34c8f5d231671e2a919a.MainApplication.class, md54f2f657d11db34c8f5d231671e2a919a.MainApplication.__md_methods);
+		
+	}
+}
---
+++ Signature/Signature/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ Se descargó nuget Xam.Plugin.Media v 2.3.0 en la solucion
+
+ */
+using SignaturePad.Forms;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Signature
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+
+            Stream image = await PadView.GetImageStreamAsync(SignatureImageFormat.Png);
+            
+        }
+
+        private void ClearButton_Clicked(object sender, EventArgs e)
+        {
+            PadView.Clear();
+        }
+    }
+}
